eb9c36e1d60a7fe80e14f0caf98c54b86ad60e7e
Orion <owolf-hubbard@catalyte.io>
2018-07-25 08:49:50 -0400

    added Authenticate.java

--- src/main/java/com/catalyte/OrionsPets/repositories/UserRepository.java
+++ src/main/java/com/catalyte/OrionsPets/repositories/UserRepository.java
@@ -5,4 +5,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface UserRepository extends MongoRepository<User, String> {
 
+  boolean existsByUsername(String username);
+
+  User findOneByUsername(String username);
 }
---
+++ src/main/java/com/catalyte/OrionsPets/services/Authenticate.java
@@ -0,0 +1,23 @@
+package com.catalyte.OrionsPets.services;
+
+import com.catalyte.OrionsPets.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Authenticate {
+
+  UserRepository userRepository;
+
+  @Autowired
+  public Authenticate(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+
+  public boolean authenticate(String username, String password, String role) {
+    return userRepository.existsByUsername(username) &&
+        userRepository.findOneByUsername(username).getPassword().equals(password) &&
+        userRepository.findOneByUsername(username).hasRole(role) ;
+  }
+
+}

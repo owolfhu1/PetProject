9d601962edd9432042cafa95e40ee79ce441f7cb
Orion <owolf-hubbard@catalyte.io>
2018-07-26 10:17:32 -0400

    PetService + PetServiceTest compleate

--- src/main/java/com/catalyte/OrionsPets/services/PetService.java
+++ src/main/java/com/catalyte/OrionsPets/services/PetService.java
@@ -17,108 +17,125 @@ import java.util.List;
 @Service
 public class PetService {
 
-    private PetTypeRepository petTypeRepository;
-    private InventoryRepository inventoryRepository;
-    private PetRepository petRepository;
+  private PetTypeRepository petTypeRepository;
+  private InventoryRepository inventoryRepository;
+  private PetRepository petRepository;
 
-    @Autowired
-    public PetService(PetTypeRepository petTypeRepository,
-                      InventoryRepository inventoryRepository,
-                      PetRepository petRepository) {
-        this.petTypeRepository = petTypeRepository;
-        this.petRepository = petRepository;
-        this.inventoryRepository = inventoryRepository;
+  @Autowired
+  public PetService(PetTypeRepository petTypeRepository,
+      InventoryRepository inventoryRepository,
+      PetRepository petRepository) {
+    this.petTypeRepository = petTypeRepository;
+    this.petRepository = petRepository;
+    this.inventoryRepository = inventoryRepository;
+  }
+
+
+  public List<Pet> searchPets(String type, String value) {
+    List<Pet> list;
+    switch (type) {
+      case "type":
+        if (petTypeRepository.existsByType(value))
+          list = petRepository.findByPetTypeId(petTypeRepository.findByType(value).getId());
+        else
+          list = new ArrayList<>();
+        break;
+      case "name":
+        list = petRepository.findByName(value);
+        break;
+      case "color":
+        list = petRepository.findByColor(value);
+        break;
+      case "sex":
+        list = petRepository.findBySex(value);
+        break;
+      case "sold":
+        list = (value.equals("true") || value.equals("false")) ?
+            petRepository.findBySold(value.equals("true")) : new ArrayList<>();
+        break;
+      case "age":
+        list = value.matches("^[0-9]+$") ?
+            petRepository.findByAge(Integer.parseInt(value)) : new ArrayList<>();
+        break;
+      default:
+        list = new ArrayList<>();
     }
+    return list;
+  }
+
+  public String createPet(Pet pet) {
+    String validation = validatePet(pet);
+    if (validation.isEmpty()) {
+      String petTypeId = petTypeRepository.findByType(pet.getPetTypeId()).getId();
+      pet.setPetTypeId(petTypeId);
+      Inventory inventory = inventoryRepository.findByPetTypeId(petTypeId);
+      InventoryDTO invDTO = new InventoryDTO(inventory);
+      invDTO.addInventory(1);
+      petRepository.save(pet);
+      inventoryRepository.save(inventory);
+      return "Pet created";
+    } else return validation;
+  }
+
+  public String updatePet(Pet pet) {
+    if (!petRepository.existsById(pet.getId()))
+      return "Could not find pet to update. (bad petId)";
+    String validation = validatePet(pet);
+    if (validation.isEmpty()) {
+      Pet oldPet = petRepository.findOneById(pet.getId());
+      String petTypeId = petTypeRepository.findByType(pet.getPetTypeId()).getId();
+      pet.setPetTypeId(petTypeId);
+      petRepository.save(pet);
+      if (oldPet.isSold() != pet.isSold()) {
+        Inventory inventory = inventoryRepository.findByPetTypeId(petTypeId);
+        InventoryDTO invDto = new InventoryDTO(inventory);
+        invDto.addInventory(pet.isSold() ? -1 : 1);
+        inventoryRepository.save(inventory);
+      }
+      return "Pet updated";
+    } else return validation;
+  }
+
+  public String deletePet(String petId) {
+    if (petRepository.existsById(petId)){
+      Pet pet = petRepository.findOneById(petId);
+      if (!pet.isSold()) {
+        Inventory inventory = inventoryRepository
+            .findByPetTypeId(pet.getPetTypeId());
+        InventoryDTO invDto = new InventoryDTO(inventory);
+        invDto.addInventory(-1);
+        inventoryRepository.save(inventory);
+      }
+      petRepository.deleteById(petId);
+      return "Pet deleted";
+    } else return "Pet to delete not found";
+  }
 
 
-    public List<Pet> searchPets(String type, String value) {
-        List<Pet> list;
-        switch (type) {
-            case "type" :
-                if(petTypeRepository.existsByType(value))
-                    list = petRepository.findByPetTypeId(petTypeRepository.findByType(value).getId());
-                else
-                    list = new ArrayList<>();
-                break;
-            case "name" :
-                list = petRepository.findByName(value);
-                break;
-            case "color" :
-                list = petRepository.findByColor(value);
-                break;
-            case "sex" :
-                list = petRepository.findBySex(value);
-                break;
-            case "sold" :
-                list = (value.equals("true") || value.equals("false")) ?
-                        petRepository.findBySold(value.equals("true")) : new ArrayList<>();
-                break;
-            case "age" :
-                list = value.matches("^[0-9]+$") ?
-                        petRepository.findByAge(Integer.parseInt(value)) : new ArrayList<>();
-                break;
-            default:
-                list = new ArrayList<>();
-        }
-        return list;
-    }
+  private String validatePet(Pet pet) {
 
-    public String createPet(Pet pet, String type) {
-        String validation = validatePet(pet,type);
-        if (validation.isEmpty()) {
-            String petTypeId = petTypeRepository.findByType(type).getId();
-            pet.setPetTypeId(petTypeId);
-            Inventory inventory = inventoryRepository.findByPetTypeId(petTypeId);
-            InventoryDTO invDTO = new InventoryDTO(inventory);
-            invDTO.addInventory(1);
-            petRepository.save(pet);
-            inventoryRepository.save(inventory);
-            return "Pet created";
-        }
-        else return validation;
-    }
+    if (!petTypeRepository.existsByType(pet.getPetTypeId()))
+      return "Bad pet type";
 
-    public String updatePet(Pet pet) {
-        return null;
-        //TODO
-    }
+    if (pet.getName().isEmpty() || pet.getName().split(" ").length == 0)
+      return "Pet name required";
 
-    public String deletePet(String petId) {
-        return null;
-        //TODO
-    }
+    if (pet.getName().length() > 15)
+      return "Pet name too long, max 15 char.";
 
+    if (pet.getAge() < 0)
+      return "Pet must have a positive age!!";
 
+    if (pet.getColor().isEmpty() || pet.getColor().split(" ").length == 0)
+      return "Pet color required";
 
+    if (pet.getColor().length() > 15)
+      return "Pet color too long, max 15 char.";
 
+    if (!pet.getSex().equals("male") && !pet.getSex().equals("female"))
+      return "Pet sex must be male or female";
 
-
-
-
-    private String validatePet(Pet pet, String type) {
-
-        if (!petTypeRepository.existsByType(type))
-            return "Bad pet type";
-
-        if (pet.getName().isEmpty() || pet.getName().split(" ").length == 0)
-            return "Pet name required";
-
-        if (pet.getName().length() > 15)
-            return "Pet name too long, max 15 char.";
-
-        if (pet.getAge() < 0)
-            return "Pet must have a positive age!!";
-
-        if (pet.getColor().isEmpty() || pet.getColor().split(" ").length == 0)
-            return "Pet color required";
-
-        if (pet.getColor().length() > 15)
-            return "Pet color too long, max 15 char.";
-
-        if (!pet.getSex().equals("male") && !pet.getSex().equals("female"))
-            return "Pet sex must be male or female";
-
-        return "";
-    }
+    return "";
+  }
 
 }
--- src/test/java/com/catalyte/OrionsPets/services/PetServiceTest.java
+++ src/test/java/com/catalyte/OrionsPets/services/PetServiceTest.java
@@ -21,82 +21,86 @@ import static org.mockito.MockitoAnnotations.initMocks;
 
 public class PetServiceTest {
 
-    private PetService classToTest;
+  private PetService classToTest;
 
-    @Mock
-    private PetTypeRepository typeRepoMock;
-    @Mock
-    private PetRepository petRepoMock;
-    @Mock
-    private InventoryRepository invRepoMock;
+  @Mock
+  private PetTypeRepository typeRepoMock;
+  @Mock
+  private PetRepository petRepoMock;
+  @Mock
+  private InventoryRepository invRepoMock;
 
-    @Before
-    public void before() {
-        initMocks(this);
-        classToTest = new PetService(typeRepoMock, invRepoMock, petRepoMock);
-    }
+  @Before
+  public void before() {
+    initMocks(this);
+    classToTest = new PetService(typeRepoMock, invRepoMock, petRepoMock);
+  }
 
-    @Test
-    public void searchPetsHappyPath() {
-        ArrayList<Pet> pets = new ArrayList<>();
-        pets.add(new Pet("this","is",1,"dumb","pet"));
-        String value = "example";
-        String type = "name";
-        doReturn(pets).when(petRepoMock).findByName(value);
-        List<Pet> result = classToTest.searchPets(type,value);
-        assertEquals(pets,result);
-    }
+  @Test
+  public void searchPetsHappyPath() {
+    ArrayList<Pet> pets = new ArrayList<>();
+    pets.add(new Pet("this", "is", 1, "dumb", "pet"));
+    String value = "example";
+    String type = "name";
+    doReturn(pets).when(petRepoMock).findByName(value);
+    List<Pet> result = classToTest.searchPets(type, value);
+    assertEquals(pets, result);
+  }
 
-    @Test
-    public void searchPetsSadPath() {
-        assertEquals(new ArrayList<Pet>(), classToTest.searchPets("badType","value"));
-    }
+  @Test
+  public void searchPetsSadPath() {
+    assertEquals(new ArrayList<Pet>(), classToTest.searchPets("badType", "value"));
+  }
 
-    @Test
-    public void createPetHappyPath() {
-        Pet pet = new Pet("petTypeId","name",1,"color","male");
-        String type = "dog";
-        PetType petType = new PetType("dog");
-        petType.setId("DUMMY_ID_STRING_1234");
-        doReturn(true).when(typeRepoMock).existsByType(type);
-        doReturn(petType).when(typeRepoMock).findByType(type);
-        doReturn(new Inventory()).when(invRepoMock).findByPetTypeId(petType.getId());
-        String result = classToTest.createPet(pet,type);
-        assertEquals("Pet created", result);
-    }
+  @Test
+  public void createPetHappyPath() {
+    Pet pet = new Pet("dog", "name", 1, "color", "male");
+    PetType petType = new PetType("dog");
+    petType.setId("DUMMY_ID_STRING_1234");
+    doReturn(true).when(typeRepoMock).existsByType(pet.getPetTypeId());
+    doReturn(petType).when(typeRepoMock).findByType(pet.getPetTypeId());
+    doReturn(new Inventory()).when(invRepoMock).findByPetTypeId(petType.getId());
+    String result = classToTest.createPet(pet);
+    assertEquals("Pet created", result);
+  }
 
-    @Test
-    public void createPetSadPath() {
-        Pet pet = new Pet("", "dd", -1, "blue", "male");
-        String type = "";
-        PetType petType = new PetType("");
-        petType.setId("");
-        doReturn(true).when(typeRepoMock).existsByType(type);
-        doReturn(petType).when(typeRepoMock).findByType(type);
-        doReturn(new Inventory()).when(invRepoMock).findByPetTypeId(petType.getId());
-        String result = classToTest.createPet(pet,type);
-        assertNotEquals("Pet created", result);
-    }
+  @Test
+  public void createPetSadPath() {
+    Pet pet = new Pet("", "dd", -1, "blue", "male");
+    PetType petType = new PetType("");
+    petType.setId("");
+    doReturn(true).when(typeRepoMock).existsByType(pet.getPetTypeId());
+    doReturn(petType).when(typeRepoMock).findByType(pet.getPetTypeId());
+    doReturn(new Inventory()).when(invRepoMock).findByPetTypeId(petType.getId());
+    String result = classToTest.createPet(pet);
+    assertNotEquals("Pet created", result);
+  }
 
-    @Test
-    public void updatePetHappyPath() {
-        //TODO
-    }
+  @Test
+  public void updatePetHappyPath() {
+    Pet pet = new Pet("dog", "name", 1, "color", "male");
+    pet.setId("DUMMY_ID_STRING_4321");
+    PetType petType = new PetType("dog");
+    petType.setId("DUMMY_ID_STRING_1234");
+    doReturn(true).when(typeRepoMock).existsByType(pet.getPetTypeId());
+    doReturn(petType).when(typeRepoMock).findByType(pet.getPetTypeId());
+    doReturn(new Inventory()).when(invRepoMock).findByPetTypeId(petType.getId());
+    doReturn(true).when(petRepoMock).existsById(pet.getId());
+    doReturn(pet).when(petRepoMock).findOneById(pet.getId());
+    String result = classToTest.updatePet(pet);
+    assertEquals("Pet updated", result);
+  }
 
-    @Test void updatePetSadPath() {
-        //TODO
-    }
-
-    @Test
-    public void deletePetHappyPath() {
-        //TODO
-    }
-
-    @Test
-    public void deletePetSadPath() {
-        //TODO
-    }
-
+  @Test
+  public void deletePetHappyPath() {
+    Pet pet = new Pet();
+    pet.setId("idString");
+    pet.setSold(true);
+    doReturn(true).when(petRepoMock).existsById(pet.getId());
+    doReturn(pet).when(petRepoMock).findOneById(pet.getId());
+    String result = classToTest.deletePet(pet.getId());
+    assertEquals("Pet deleted", result);
+  }
 
 }
 
